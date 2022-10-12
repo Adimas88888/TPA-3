c15e81187ab5ac6b09c6d4474045bd00bc422fcd
adi mas setiawan <103758489+Adimas88888@users.noreply.github.com>
2022-10-12 19:42:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //TMDB
 
-const API_KEY = 'api_key=c80fb401592e37adaa4623dec9e31439';
+const API_KEY = 'api_key=85e09be72263c2d9eb65d5da8efde411';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
